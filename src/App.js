114d97a761dd0013e6b8c8fe94d222c1e98733a9
GMohammadAli <gulammohammadali@gmail.com>
2022-06-21 01:22:25 +0530

    Fetch Call changed to an Axios Call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
 import ImageSearch from "./components/ImageSearch";
+import axios from "axios";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -8,13 +9,14 @@ function App() {
   const [term, setTerm] = useState("");
 
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImages(data.hits);
-        setIsLoading(false);
-      })
-      .catch((err) => console.log(err));
+      axios
+        .get(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`)
+        .then((data) => {
+          console.log(data);
+          setImages(data.hits);
+          setIsLoading(false);
+        })
+        .catch((err) => console.log(err));
   }, [term]);
 
   return (
